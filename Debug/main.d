3498cbfc077d8a98e7935d00f132d618a370a9d5
Daniel Walsh <walsh.daniel@icloud.com>
2016-04-30 16:31:25 -0400

    added project files

---
+++ Debug/main.d
@@ -0,0 +1,20 @@
+main.d: ../main.c ../TM4C123GH6PM.h \
+ /Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cm4.h \
+ /Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmInstr.h \
+ /Users/danielwalsh/Documents/CCS/CMSIS/Include/cmsis_gcc.h \
+ /Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmFunc.h \
+ /Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmSimd.h ../bsp.h
+
+../TM4C123GH6PM.h:
+
+/Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cm4.h:
+
+/Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmInstr.h:
+
+/Users/danielwalsh/Documents/CCS/CMSIS/Include/cmsis_gcc.h:
+
+/Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmFunc.h:
+
+/Users/danielwalsh/Documents/CCS/CMSIS/Include/core_cmSimd.h:
+
+../bsp.h:
